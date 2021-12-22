9166b5d1c6538ec2f3ec472078dae12c6fadf57e
robolovo <dlwnstj21@naver.com>
2021-12-22 12:49:19 +0900

    chore: 백엔드 기본 환경 설정

---
+++ backend/src/main/java/com/golfie/GolfieApplication.java
@@ -0,0 +1,13 @@
+package com.golfie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolfieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GolfieApplication.class, args);
+    }
+
+}
