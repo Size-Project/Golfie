ea88c0a1cd6a4723b4a7bdba491e360f0f159146
김진아 <wlsdk7245@gmail.com>
2022-01-02 14:53:59 +0900

    chore: 리덕스, API 모듈화 작업

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import App from "./App";
+import rootReducer from './store';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
     <React.StrictMode>
-        <App/>
+        <Provider store={store}>
+            <App/>
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root'),
 );
