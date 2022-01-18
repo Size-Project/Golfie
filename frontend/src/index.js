17757c560580e38277553d0c6d25e86108982fc4
김진아 <wlsdk7245@gmail.com>
2022-01-18 20:49:39 +0900

    feat: 글로벌 스타일 설정

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
-import {composeWithDevTools} from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import AppRouter from './AppRouter';
 import rootReducer from './services';
+import { GlobalStyle } from './GlobalStyle';
 
 const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+      <GlobalStyle />
       <AppRouter />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
