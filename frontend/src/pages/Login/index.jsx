832a1c9f90baa5f68c896c854d554cd5c9b32783
lotus9630 <lotus9630@gmail.com>
2022-01-19 11:24:37 +0900

    로그인 화면 퍼블리싱 완료

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,14 +1,39 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const Wrapper = styled.div`
-  border: 1px solid;
-  padding: 10px;
-  width: 300px;
-`;
+import { Wrapper } from './styled';
+import mainIcon from 'assets/icons/mainIcon.png';
+import googleLoginIcon from 'assets/icons/googleLoginIcon.png';
+import naverLoginIcon from 'assets/icons/naverLoginIcon.png';
+import kakaoLoginIcon from 'assets/icons/kakaoLoginIcon.png';
 
 const LoginPage = () => {
-  return <Wrapper>login Page</Wrapper>;
+  return (
+    <Wrapper>
+      <header className="home">
+        <img src={mainIcon} alt="홈 아이콘" className="home-icon" />
+        <div>fieldtrip</div>
+      </header>
+      <section className="login-box">
+        <div className="icons">
+          <img
+            src={naverLoginIcon}
+            alt="네이버 로그인 아이콘"
+            className="login-icon"
+          />
+          <img
+            src={kakaoLoginIcon}
+            alt="카카오 로그인 아이콘"
+            className="login-icon"
+          />
+          <img
+            src={googleLoginIcon}
+            alt="구글 로그인 아이콘"
+            className="login-icon"
+          />
+        </div>
+        <div className="home-button">로그인하지 않고 홈으로 이동하기</div>
+      </section>
+    </Wrapper>
+  );
 };
 
 export default LoginPage;
