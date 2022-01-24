8863565e2696b8b1028bd8856e15c15cd615bbf6
lotus9630 <lotus9630@gmail.com>
2022-01-24 23:38:04 +0900

    카카오 로그인 연결

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -4,8 +4,19 @@ import mainIcon from 'assets/icons/mainIcon.png';
 import googleLoginIcon from 'assets/icons/googleLoginIcon.png';
 import naverLoginIcon from 'assets/icons/naverLoginIcon.png';
 import kakaoLoginIcon from 'assets/icons/kakaoLoginIcon.png';
+import axios from 'axios';
+// REST API KEY = 0e7846ce2b8ea8f27a487ba067a3d527
+// GET /oauth/authorize?client_id={REST_API_KEY}&redirect_uri={REDIRECT_URI}&response_type=code HTTP/1.1
+// Host: kauth.kakao.com
+axios.defaults.withCredentials = true;
 
 const LoginPage = () => {
+  const client_id = '0e7846ce2b8ea8f27a487ba067a3d527';
+  const redirect_uri = 'http://localhost:3000/account/login';
+  const response_type = 'code';
+  const kakaoLink = `https://kauth.kakao.com/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=${response_type}
+  `;
+  const handleClick = async () => {};
   return (
     <Wrapper>
       <header className="home">
@@ -16,16 +27,21 @@ const LoginPage = () => {
         <div className="icons">
           <img
             src={naverLoginIcon}
+            onClick={handleClick}
             alt="네이버 로그인 아이콘"
             className="login-icon"
           />
-          <img
-            src={kakaoLoginIcon}
-            alt="카카오 로그인 아이콘"
-            className="login-icon"
-          />
+          <a href={kakaoLink}>
+            <img
+              src={kakaoLoginIcon}
+              onClick={handleClick}
+              alt="카카오 로그인 아이콘"
+              className="login-icon"
+            />
+          </a>
           <img
             src={googleLoginIcon}
+            onClick={handleClick}
             alt="구글 로그인 아이콘"
             className="login-icon"
           />
