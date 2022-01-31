15b4a6b8fdf1aac689709858d2e270cfabf9b215
김진아 <wlsdk7245@gmail.com>
2022-01-31 14:19:17 +0900

    chore: 코드 정리

--- frontend/src/layout/Join/JoinRecommend.jsx
+++ frontend/src/layout/Join/JoinRecommend.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
-import { StyledJoinRecommend } from './styled';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-
-import SwiperCore, { Navigation, Pagination } from 'swiper';
 import JoinCardHorizontal from '../../component/JoinCard/JoinCardHorizontal';
-SwiperCore.use([Navigation, Pagination]);
+import { StyledJoinRecommend } from './styled';
 
 const JoinRecommend = () => {
   return (
