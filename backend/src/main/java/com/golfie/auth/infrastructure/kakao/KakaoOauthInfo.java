97336f7ed1800dfd49ca712baab919078a9baf89
robolovo <dlwnstj21@naver.com>
2022-02-06 20:50:10 +0900

    fix: OAuth 회원가입 및 로그인 redirect-uri 두개로 변경

--- backend/src/main/java/com/golfie/auth/infrastructure/kakao/KakaoOauthInfo.java
+++ backend/src/main/java/com/golfie/auth/infrastructure/kakao/KakaoOauthInfo.java
@@ -6,18 +6,25 @@ import org.springframework.stereotype.Component;
 @Component
 public class KakaoOauthInfo {
     public static String clientId;
-    public static String redirectUri;
+    public static String signupRedirectUri;
+    public static String loginRedirectUri;
     public static String userInfoRequestUri;
     public static String accessTokenRequestUri;
+    public static String redirectUri;
 
     @Value("${oauth.kakao.client-id}")
     public void setClientId(String clientId) {
         KakaoOauthInfo.clientId = clientId;
     }
 
-    @Value("${oauth.kakao.redirect-uri}")
-    public void setRedirectUri(String redirectUri) {
-        KakaoOauthInfo.redirectUri = redirectUri;
+    @Value("${oauth.kakao.signup-redirect-uri}")
+    public void setSignupRedirectUri(String redirectUri) {
+        KakaoOauthInfo.signupRedirectUri = redirectUri;
+    }
+
+    @Value("${oauth.kakao.login-redirect-uri}")
+    public void setLoginRedirectUri(String redirectUri) {
+        KakaoOauthInfo.loginRedirectUri = redirectUri;
     }
 
     @Value("${oauth.kakao.user-info-request-uri}")
@@ -28,5 +35,9 @@ public class KakaoOauthInfo {
     @Value("${oauth.kakao.access-token-request-uri}")
     public void setAccessTokenRequestUri(String accessTokenRequestUri) {
         KakaoOauthInfo.accessTokenRequestUri = accessTokenRequestUri;
+    }
+
+    public static void setRedirectUri(String uri) {
+        redirectUri = uri;
     }
 }
