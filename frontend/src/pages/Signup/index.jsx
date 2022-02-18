5f0153b3755bdb2124e023e57cadf4c158de5a10
lotus9630 <lotus9630@gmail.com>
2022-02-19 05:50:19 +0900

    feature: 회원가입 유저 스타일 퍼블리싱

--- frontend/src/pages/Signup/index.jsx
+++ frontend/src/pages/Signup/index.jsx
@@ -4,12 +4,13 @@ import axios from 'axios';
 import { setCookie } from 'utils/cookie';
 import API from 'api';
 import kakaoLoginIcon from 'assets/icons/kakaoLoginIcon.png';
+import checkIcon from 'assets/icons/checkIcon.png';
 import { Wrapper } from './styled';
 import SignUpHeader from 'layout/SignUpHeader';
 
 const SignupPage = (a) => {
   const [nickname, setNickname] = useState('');
-  const [buttonOff, setButtonOff] = useState(true);
+  const [nickCheck, setNickCheck] = useState(true);
   const [user, setUser] = useState({});
   const location = useLocation();
   const navigate = useNavigate();
@@ -22,7 +23,7 @@ const SignupPage = (a) => {
     const response = await axios.post('/api/validate/nickname', {
       nickname: value,
     });
-    if (response.status === 200) setButtonOff(false);
+    if (response.status === 200) setNickCheck(false);
   };
 
   const handleClick = async (e) => {
@@ -60,15 +61,133 @@ const SignupPage = (a) => {
           className="login-icon"
         />
       </a>
-      <form action="">
-        <input
-          type="text"
-          value={nickname}
-          className="text"
-          placeholder="닉네임"
-          onChange={nicknameConfirm}
-        />
-        <button type="submit" disabled={buttonOff} onClick={handleClick}>
+      <form action="" className="signup-form">
+        <section className="input-text">
+          <div className="input-header">
+            <img src={checkIcon} alt="" />
+            <div className="input-title">닉네임</div>
+          </div>
+          <input
+            type="text"
+            value={nickname}
+            className="text"
+            placeholder="입력해주세요"
+            onChange={nicknameConfirm}
+          />
+          {nickCheck && (
+            <div className="error-message">닉네임이 잘못되었습니다</div>
+          )}
+        </section>
+        <section className="input-text">
+          <div className="input-header">
+            <img src={checkIcon} alt="" />
+            <div className="input-title">직업</div>
+          </div>
+          <input
+            type="text"
+            value={nickname}
+            className="text"
+            placeholder="입력해주세요"
+            onChange={nicknameConfirm}
+          />
+        </section>
+        <section className="input-text">
+          <div className="input-header">
+            <img src={checkIcon} alt="" />
+            <div className="input-title">타수</div>
+          </div>
+          <input
+            type="text"
+            value={nickname}
+            className="text"
+            placeholder="입력해주세요"
+            onChange={nicknameConfirm}
+          />
+        </section>
+
+        <section className="input-select">
+          <div className="input-select-title">함께하고 싶은 평균 타수</div>
+          <div className="select-item-box">
+            <div className="select-item">
+              <div className="item-text">126타 이상</div>
+            </div>
+            <div className="select-item">
+              <div className="item-text">100타 이상</div>
+            </div>
+            <div className="select-item">
+              <div className="item-text">90타 전후</div>
+            </div>
+            <div className="select-item">
+              <div className="item-text">72 - 81타</div>
+            </div>
+          </div>
+        </section>
+        <section className="input-select">
+          <div className="input-select-title">함께하고 싶은 연령대</div>
+          <div className="select-item-box">
+            <div className="select-item">
+              <div className="item-text">20세 미만</div>
+            </div>
+            <div className="select-item">
+              <div className="item-text">21 - 25세</div>
+            </div>
+            <div className="select-item">
+              <div className="item-text">26 - 30세</div>
+            </div>
+            <div className="select-item">
+              <div className="item-text">31 - 35세</div>
+            </div>
+            <div className="select-item">
+              <div className="item-text">36 - 40세</div>
+            </div>
+            <div className="select-item">
+              <div className="item-text">40세 이상</div>
+            </div>
+          </div>
+        </section>
+        <section className="input-select">
+          <div className="input-select-title">좋아하는 분위기</div>
+          <div className="select-item-box">
+            <div className="select-item">
+              <div className="item-text">편안한</div>
+            </div>
+            <div className="select-item">
+              <div className="item-text">즐거운</div>
+            </div>
+            <div className="select-item">
+              <div className="item-text">열정적인</div>
+            </div>
+            <div className="select-item">
+              <div className="item-text">교훈적인</div>
+            </div>
+            <div className="select-item">
+              <div className="item-text">가벼운</div>
+            </div>
+            <div className="select-item">
+              <div className="item-text">진지한</div>
+            </div>
+            <div className="select-item">
+              <div className="item-text">칭찬하는</div>
+            </div>
+            <div className="select-item">
+              <div className="item-text">침착한</div>
+            </div>
+            <div className="select-item">
+              <div className="item-text">예의바른</div>
+            </div>
+            <div className="select-item">
+              <div className="item-text">긍정적인</div>
+            </div>
+            <div className="select-item">
+              <div className="item-text">조용한</div>
+            </div>
+            <div className="select-item">
+              <div className="item-text">존중하는</div>
+            </div>
+          </div>
+        </section>
+
+        <button type="submit" disabled={nickCheck} onClick={handleClick}>
           Join
         </button>
       </form>
--- frontend/src/pages/Signup/styled.jsx
+++ frontend/src/pages/Signup/styled.jsx
@@ -1,3 +1,66 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  .signup-form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .input-text {
+    width: 335px;
+    /* margin: auto; */
+    margin-bottom: 16px;
+    .input-header {
+      display: flex;
+      margin-bottom: 12px;
+      img {
+        margin-right: 5px;
+      }
+      .input-title {
+        font-size: 16px;
+        font-weight: bold;
+        color: var(--color-sub);
+      }
+    }
+    input {
+      width: 100%;
+      border: 0;
+      border-bottom: solid 1px;
+      border-color: #c1c1c1;
+      margin-bottom: 3px;
+    }
+    .error-message {
+      font-size: 8px;
+      font-weight: 500;
+      color: #f7c844;
+    }
+  }
+  .input-select {
+    width: 335px;
+    margin-bottom: 50px;
+    .input-select-title {
+      font-siez: 16px;
+      font-weight: bold;
+      color: #3bd48e;
+      margin-bottom: 20px;
+    }
+    .select-item-box {
+      display: flex;
+      flex-wrap: wrap;
+      .select-item {
+        height: 33px;
+        border-radius: 16px;
+        border: solid 1px #3bd48e;
+        display: flex;
+        padding: 6px 15px 6px 15px;
+        margin-right: 6px;
+        margin-bottom: 10px;
+
+        .item-text {
+          margin: auto;
+        }
+      }
+    }
+  }
+`;
